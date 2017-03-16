639a72397f2ef3818dff4d20a27d8bec1d97a4cb
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-03-16 18:07:14 +0200

    [ref]

--- Tests/PlacesTests/PlacesTests.swift
+++ Tests/PlacesTests/PlacesTests.swift
@@ -2,16 +2,13 @@ import XCTest
 @testable import Places
 
 class PlacesTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Places().text, "Hello, World!")
+    func testPlace() {
+        XCTAssertNil(Place().name)
     }
-
 
     static var allTests : [(String, (PlacesTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testPlace", testPlace),
         ]
     }
 }
