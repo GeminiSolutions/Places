ea61177db3a1b9a1e7834eb54ab677c9a49f81a5
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-05-12 15:23:12 +0300

    [fix]

--- Sources/PlacesList.swift
+++ Sources/PlacesList.swift
@@ -14,7 +14,7 @@ public class PlacesList: DataStoreContentJSONArray<[String:Any]> {
     }
 
     public init(placesData: [(place: Place.JSONObjectType, id: Place.PlaceIdType, lastUpdate: Date)]) {
-        let places = placesData.map({ return ["place":$0.place, "id":Place.stringFromPlaceId($0.id), "lastUpdate":PlacesList.dateString(from: $0.lastUpdate)] })
+        let places = placesData.map({ return ["content":$0.place, "id":Place.stringFromPlaceId($0.id), "lastUpdate":PlacesList.dateString(from: $0.lastUpdate)] })
         super.init(json: places)
     }
 
@@ -37,11 +37,11 @@ public class PlacesList: DataStoreContentJSONArray<[String:Any]> {
 
     public func append(place: Place) {
         guard let id = place.id, let lastUpdate = place.lastUpdate else { return }
-        append(["place":place.content, "id":Place.stringFromPlaceId(id), "lastUpdate":PlacesList.dateString(from: lastUpdate)])
+        append(["content":place.content, "id":Place.stringFromPlaceId(id), "lastUpdate":PlacesList.dateString(from: lastUpdate)])
     }
 
     private func place<PlaceType: Place>(from data: [String:Any]) -> PlaceType? {
-        guard let content = data["place"] as? Place.JSONObjectType, let id = data["id"] as? String, let lastUpdate = data["lastUpdate"] as? String else { return nil }
+        guard let content = data["content"] as? Place.JSONObjectType, let id = data["id"] as? String, let lastUpdate = data["lastUpdate"] as? String else { return nil }
         guard let placeId = Place.placeIdFromString(id), let placeLastUpdate = PlacesList.date(from: lastUpdate) else { return nil }
         guard let place = PlaceType(content: content) else { return nil }
         place.id = placeId
